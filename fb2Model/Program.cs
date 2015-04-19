c90f60b26b0ccb8ca0d3d6511d087915a4bfc16c
SergeyFilippov <sergey.filippov@outlook.com>
2015-04-19 21:33:17 +0200

    Testing improved

--- fb2Model/Program.cs
+++ fb2Model/Program.cs
@@ -1,7 +1,9 @@
 ﻿namespace fb2Console
 {
     using System;
+    using System.Diagnostics;
     using System.IO;
+    using System.Threading.Tasks;
     using System.Xml;
     using System.Xml.Serialization;
 
@@ -20,14 +22,88 @@
         /// </param>
         private static void Main(string[] args)
         {
-            var book =
-                Deserialize<FictionBook>(
-                    @"C:\Users\fenri_000\Desktop\Игры\ФБ\Kamenistyiyi_A._Rayibesposhadnyiyi1._Rayi_Besposhadnyiyi.fb2");
-            var titleInfo = book.description.titleinfo;
-            Console.WriteLine(titleInfo.booktitle);
-            Console.WriteLine(titleInfo.author[0].Items[0] + " " + titleInfo.author[0].Items[1]);
-            Console.WriteLine(titleInfo.date.value);
+
+            var path = @"D:\Библиотека";
+            var files = Directory.GetFiles(path, "*.fb2", SearchOption.AllDirectories);
+
+            Console.WriteLine("Total files found: " + files.Length);
+
+            ProcessParallel(files);
+            Process(files);
+            
             Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Processes the specified files.
+        /// </summary>
+        /// <param name="files">The files.</param>
+        private static void Process(string[] files)
+        {
+            var counter = new Stopwatch();
+
+            Console.WriteLine("Processing files in one thread.");
+
+            counter.Start();
+
+            foreach (var file in files)
+            {
+                ProcessFile(file);
+            }
+
+            counter.Stop();
+
+            Console.WriteLine("Elapsed time to process all books:" + counter.Elapsed.ToString("g"));
+            Console.WriteLine("Time per book" + new TimeSpan(counter.ElapsedTicks / files.Length).ToString("g"));
+        }
+
+        /// <summary>
+        /// Processes the parallel.
+        /// </summary>
+        /// <param name="files">The files.</param>
+        private static void ProcessParallel(string[] files)
+        {
+            var counter = new Stopwatch();
+
+            Console.WriteLine("Processing files in parallel.");
+
+            counter.Start();
+
+            Parallel.ForEach(
+                files,
+                (file) =>
+                    {
+                        ProcessFile(file);
+                    });
+
+            counter.Stop();
+
+            Console.WriteLine("Elapsed time to process all books:" + counter.Elapsed.ToString("g"));
+            Console.WriteLine("Time per book" + new TimeSpan(counter.ElapsedTicks / files.Length).ToString("g"));
+        }
+
+        /// <summary>
+        /// Processes the file.
+        /// </summary>
+        /// <param name="file">The file.</param>
+        private static void ProcessFile(string file)
+        {
+            try
+            {
+                var book = Deserialize<FictionBook>(file);
+                var titleInfo = book.description.titleinfo;
+                //Console.WriteLine(titleInfo.booktitle);
+                //Console.WriteLine("\t" + titleInfo.author[0].Items[0] + " " + titleInfo.author[0].Items[1]);
+                //Console.WriteLine("\t" + titleInfo.date);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error during reading: " + file);
+            }
+            finally
+            {
+                //Console.WriteLine();
+            }
         }
 
         /// <summary>
@@ -49,7 +125,7 @@
             }
 
             var xmlSerializer = new XmlSerializer(typeof(T));
-            using (var stream = new FileStream(filePath, FileMode.Open))
+            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
             {
                 var settings = new XmlReaderSettings
                                    {
